258f3a29aad140769db436a99d41c09a1981e7d6
Maelle Vance <maellevance@gmail.com>
2016-07-25 19:15:51 -0700

    setup sales pages and headway on js

--- js/app.js
+++ js/app.js
@@ -1,6 +1,5 @@
 'use strict';
 var hours = ['6am: ', '7am: ', '8am: ', '9am: ', '10am: ', '11am: ', '12pm: ', '1pm: ', '2pm: ', '3pm: ', '4pm: ', '5pm: ', '6pm: '];
-var i = 0;
 // Alki store object :
 var alki = {
   minCust: 2,
@@ -8,11 +7,34 @@ var alki = {
   avgCookie: 4.6,
   avgCust: [],
   alkiArray: [],
+  totalCookies: 0,
+  finalArray: [],
   name : 'Alki',
-  numCustHourly: function() {
-    for (i = 0; i < hours.length; i++ ) {
-      alki.avgCust.push(Math.floor(Math.random() * (alki.maxCust - alki.minCust + 1)) + alki.minCust);
-    }
-  }
-
 };
+
+  // methods :
+alki.numCustHourly = function() {
+  for (var i = 0; i < hours.length; i++ ) {
+    this.avgCust.push(Math.floor(Math.random() * (this.maxCust - this.minCust + 1)) + this.minCust);
+  }
+};
+
+alki.cookiesPerCust = function() {
+  this.numCustHourly();
+  for (var i = 0; i < hours.length; i++) {
+    var eachCookies = Math.ceil(this.avgCust[i] * this.avgCookie);
+    this.alkiArray.push(eachCookies);
+    this.totalCookies += eachCookies;
+  }
+};
+
+
+// will take the array and display it in an unordered list
+alki.render = function() {
+  alki.cookiesPerCust();
+  for(var i = 0; i < this.alkiArray.length; i++) {
+    this.finalArray.push(hours[i] + this.alkiArray[i] + ' cookies');
+  }
+};
+
+alki.render();
