1b99147e66d50cabe4e77891eb990095986ea5c1
Maelle Vance <maellevance@gmail.com>
2016-07-26 21:30:56 -0700

    row of totals works

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@
 var hours = ['6am: ', '7am: ', '8am: ', '9am: ', '10am: ', '11am: ', '12pm: ', '1pm: ', '2pm: ', '3pm: ', '4pm: ', '5pm: ', '6pm: '];
 var myStores = [];
 var totalForEachHour = [];
+var globalDailyTotal = 0;
 
 function Store(minCust, maxCust, avgCookie, name) {
   this.minCust = minCust;
@@ -42,43 +43,36 @@ function Store(minCust, maxCust, avgCookie, name) {
 };
 
 
-var pike = new Store(23, 65, 6.3, 'First and Pike');
-var seatac = new Store(3, 24, 1.2, 'Seatac Airport');
-var center = new Store(11, 38, 3.7, 'Seattle Center');
-var capHill = new Store(20, 38, 2.3, 'Capitol Hill');
-var alki = new Store(2, 16, 4.6, 'Alki');
-
 function calcEachHour() {
   for (var hour = 0; hour < hours.length; hour ++) {
     var sum = myStores[0].cookiesSoldEachHour[hour] + myStores[1].cookiesSoldEachHour[hour] + myStores[2].cookiesSoldEachHour[hour] + myStores[3].cookiesSoldEachHour[hour] + myStores[4].cookiesSoldEachHour[hour] ;
     totalForEachHour.push(sum);
   }
+  //calculating global daily total
+  for (var store = 0; store < myStores.length; store ++) {
+    globalDailyTotal += myStores[store].totalCookies;
+  }
+  totalForEachHour.push(globalDailyTotal);
 }
 
-calcEachHour();
 
 var storeTable = document.getElementById('storeTable');
 
 function makeHeaderRow() {
-  //create row
   var tableRow = document.createElement('tr');
-  //create empty cell
   var thElement = document.createElement('th');
   tableRow.appendChild(thElement);
-  //loop through hours to create header table cells for all the hours
   for (var i = 0; i < hours.length; i++ ) {
     thElement = document.createElement('th');
     thElement.textContent = hours[i];
     tableRow.appendChild(thElement);
   };
-  //create daily location total header table cell :
   thElement = document.createElement('th');
   thElement.textContent = 'Daily Location Total:';
   tableRow.appendChild(thElement);
   storeTable.appendChild(tableRow);
 }
 
-makeHeaderRow();
 
 function makeAllStoreRows() {
   for (var store = 0; store < myStores.length; store++ ){
@@ -98,20 +92,29 @@ function makeAllStoreRows() {
   }
 };
 
-makeAllStoreRows();
 
 function makeTotalsRow() {
   var tableRow = document.createElement('tr');
   var tdElement = document.createElement('td');
   tdElement.textContent = 'Totals';
   tableRow.appendChild(tdElement);
-  for (var i = 0; i < hours.length; i++) {
+  for (var i = 0; i < totalForEachHour.length; i++) {
     tdElement = document.createElement('td');
     tdElement.textContent = totalForEachHour[i];
     tableRow.appendChild(tdElement);
   }
   storeTable.appendChild(tableRow);
-
 }
 
+//
+
+var pike = new Store(23, 65, 6.3, 'First and Pike');
+var seatac = new Store(3, 24, 1.2, 'Seatac Airport');
+var center = new Store(11, 38, 3.7, 'Seattle Center');
+var capHill = new Store(20, 38, 2.3, 'Capitol Hill');
+var alki = new Store(2, 16, 4.6, 'Alki');
+
+calcEachHour();
+makeHeaderRow();
+makeAllStoreRows();
 makeTotalsRow();
